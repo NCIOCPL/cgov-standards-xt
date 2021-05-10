d0bf4d392170a5c00dbd68cb9a3c2c877544f357
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2021-05-10 16:12:00 -0400

    (#4) Build out vanilla js config - add deps and conifgs - added new test app repository - bootstrapped with lerna

---
+++ packages/eslint-config-vanilla-js/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+		jest: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:jest/recommended",
+		"plugin:jest/style",
+		"plugin:jest-dom/recommended",
+		"plugin:prettier/recommended",
+	],
+	plugins: ["jest", "jest-dom"],
+	rules: {
+		"jest/consistent-test-it": ["error", { fn: "it" }],
+		"jest/no-if": "error",
+		"jest/no-test-return-statement": "error",
+		"jest/require-to-throw-message": "error",
+		"jest/require-top-level-describe": "error",
+	},
+};
